70d3109fbb19b39d77b70ccaebde49d1f5953966
Helvlaska <ccastan@live.fr>
2025-02-17 13:42:40 +0000

    Create 5-printf.c file : Program prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - imprime une phrase
+ *
+ * Return: 0 is succes
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

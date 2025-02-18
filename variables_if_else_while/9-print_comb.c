f2265d114f47837490e79802e799f3ea2fa65b25
Helvlaska <ccastan@live.fr>
2025-02-18 21:07:56 +0000

    Create 9-print_comb.c file : Program prints all possible combinations of single-digit numbers. Numbers must be separated by ,, followed by a space

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print numbers of 0 to 9 with space and ,
+ *
+ * Return: 0 it's ok
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		putchar(44);
+		putchar(32);
+	}
+	putchar('\n');
+	return (0);
+}

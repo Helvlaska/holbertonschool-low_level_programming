bf37e26d70a8c58260e0a03ca78d458ff85b80c8
Helvlaska <ccastan@live.fr>
2025-02-19 21:18:51 +0000

    3-islower.c & 3-main.c corrections description and syntax for betty

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,14 +1,18 @@
 #include "main.h"
 /**
- * _islower : checks for lowercase character
+ * _islower - checks if character is an lowercase
+ * @c: verified character
  *
+ * Description: This function take an character in entry
+ * and verified if an lowercase or uppercase
  *
+ * Return: 1 if character is lowercase, else 0
  */
 
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-		return(1);
+		return (1);
 	else
 		return (0);
 }

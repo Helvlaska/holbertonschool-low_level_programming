d76b7040ed90ec7ab17566e8c25019b5794f438a
Helvlaska <ccastan@live.fr>
2025-02-19 19:11:58 +0000

    Create 2-main.c 2-print_alphabet_x10.c files : Program prints 10x alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ *
+ * Return: 0 it's ok
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i <= 10;)
+	{
+		print_alphabet();
+		i++;
+	}
+}

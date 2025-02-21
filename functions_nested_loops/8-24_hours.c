7d40d6fa197bdc8cf2e76f131c43f8209d907ae2
Helvlaska <ccastan@live.fr>
2025-02-21 13:47:39 +0000

    8-24_hours.c file : add function with / and %

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -2,13 +2,23 @@
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
  *
- *
- * Description: prints every minute of the day of Jack Bauer,
- * starting from 00:00 to 23:59.
- *
- *
+ * Description: starting from 00:00 to 23:59.
  */
 
 void jack_bauer(void)
 {
+	int seconde;
+
+	for (seconde = 0; seconde < 1440; seconde++)
+	{
+		int heure = seconde / 60;
+		int minute = seconde % 60;
+
+		_putchar('0' + heure / 10);
+		_putchar('0' + heure % 10);
+		_putchar(':');
+		_putchar('0' + minute / 10);
+		_putchar('0' + minute % 10);
+		_putchar('\n');
+	}
 }

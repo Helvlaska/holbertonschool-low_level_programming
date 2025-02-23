483223f55a66da380e304a39130d8d8db98e9145
Helvlaska <ccastan@live.fr>
2025-02-23 10:10:45 +0000

    Create 10-add.c file : Program print sum

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ * @number1: it's an random number
+ * @number2: it's an random number
+ *
+ * Return: 0 it's ok
+ */
+
+int add(int number1, int number2)
+{
+	int somme = (number1 + number2);
+
+		return (somme);
+}

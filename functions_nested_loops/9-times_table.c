54cc062f461a6119b24518d3f7512336a156a839
Helvlaska <ccastan@live.fr>
2025-02-23 09:35:43 +0000

    Create 9-main.c & 9-times_table.c files : main.c check the code & times_table.c  prints the 9 times table, starting with 0

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ *
+ */
+
+void times_table(void)
+{
+	int table;
+
+	for (table = 0; table <= 9; table++)
+	{
+		int chiffre;
+
+		for (chiffre = 0; chiffre <= 9 ; chiffre++)
+		{
+			int produit = chiffre * table;
+
+			if (produit <= 9)
+			{
+				_putchar(' ');
+				_putchar('0' + produit);
+			}
+			else
+			{
+				_putchar('0' + produit / 10);
+				_putchar('0' + produit % 10);
+			}
+			if (chiffre != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}

4287a46f81de1691f48372d73f71afe839bd53a2
Helvlaska <ccastan@live.fr>
2025-02-24 13:14:14 +0000

    Create 2-mul.c file : Program multiplies two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a:integers 1
+ * @b: integers 2
+ *
+ * Return: calcul du produit
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

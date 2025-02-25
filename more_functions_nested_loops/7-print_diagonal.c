979eb669c2fb585a017d231852ba84d332cc8229
Helvlaska <ccastan@live.fr>
2025-02-25 13:31:01 +0000

    7-print_diagonal.c file correction print is a diagonal not simple symbol

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -7,16 +7,19 @@
 
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
-	if (n != 0 && n > 0)
+	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
 		{
-			_putchar(' ');
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
 		}
-		_putchar('\\');
-		_putchar('\n');
 	}
 	else
 		_putchar('\n');

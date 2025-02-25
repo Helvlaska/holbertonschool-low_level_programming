c4f072764718091b68e9a8f1f64ee466193f9e9e
Helvlaska <ccastan@live.fr>
2025-02-25 14:41:26 +0000

    Create 9-fizz_buzz.c file : Program prints the numbers from 1 to 100, followed by a new line. But for multiples of three print Fizz instead of the number and for the multiples of five print Buzz. For numbers which are multiples of both three and five print FizzBuzz.

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - prints the numbers from 1 to 100, followed by a new line.
+ * But for multiples of three print Fizz instead of the number and for the
+ * multiples of five print Buzz. For numbers which are multiples of both
+ * three and five print FizzBuzz.
+ *
+ * Return: 0 it's ok
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = 1; number <= 100; number++)
+	{
+		if ((number % 3) == 0 && (number % 5) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((number % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((number % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", number);
+		}
+	}
+	return (0);
+}

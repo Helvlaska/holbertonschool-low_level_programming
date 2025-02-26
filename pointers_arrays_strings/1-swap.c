d2b8e04f9d3d0bf4e7dfd179afa1a9b1beabe68c
Helvlaska <ccastan@live.fr>
2025-02-26 15:39:16 +0000

    Create 1-swap.c file : Program swaps the values of two integers

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: int variable
+ * @b: int variable
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+
+	*a = *b;
+	*b = temp;
+}

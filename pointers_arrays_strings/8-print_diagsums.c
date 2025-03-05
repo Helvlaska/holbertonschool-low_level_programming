db5761dfb1a044283ec9475eb016b856b13b4a9a
Helvlaska <ccastan@live.fr>
2025-03-05 14:34:15 +0000

    Create 8-print_diagsums.c file : Program  prints the sum of the two diagonals of a square matrix of integers

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,5 +1,36 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_diagsums - prints the sum of the two diagonals of a square of integers
+ * @a: pointer of array
+ * @size: size of array
+ *
  */
+
+/* array 2D : */
+
+/* {1; 2; 3}*/
+/* {4; 5; 6}*/
+/* {7; 8; 9}*/
+
+/*array mono ligne:*/
+
+/*  1     2     3     4     5     6     7     8     9*/
+/*a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8]*/
+
+void print_diagsums(int *a, int size)
+{
+	int l; /*var ligne car array mono ligne*/
+	int sum_diag_one = 0; /*somme de la diag one*/
+	int sum_diag_two = 0; /*somme de la diag two*/
+
+	/*boucle pour parcourir l'array mono ligne*/
+	for (l = 0; l < size; l++)
+	{
+		/*ex : a[0][0] : a[0 * 3 + 0] : a[0]*/
+		sum_diag_one += a[l * size + l];
+		/*ex : a[3][0] : a[3 * 3 + (3 - 1 - 3)] : a[8]*/
+		sum_diag_two += a[l * size + (size - 1 - l)];
+	}
+	printf("%d, %d\n", sum_diag_one, sum_diag_two);
+}

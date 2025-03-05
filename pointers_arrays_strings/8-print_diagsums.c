97284e89bfd1d1f98613b687ee8d9815f356b78f
Helvlaska <ccastan@live.fr>
2025-03-05 14:36:22 +0000

    8-print_diagsums.c file : Correction betty syntax

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -7,17 +7,6 @@
  *
  */
 
-/* array 2D : */
-
-/* {1; 2; 3}*/
-/* {4; 5; 6}*/
-/* {7; 8; 9}*/
-
-/*array mono ligne:*/
-
-/*  1     2     3     4     5     6     7     8     9*/
-/*a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8]*/
-
 void print_diagsums(int *a, int size)
 {
 	int l; /*var ligne car array mono ligne*/

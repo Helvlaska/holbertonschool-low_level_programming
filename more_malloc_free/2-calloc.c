b0c412ccb0d5d600a466d69826e8d9978d7765b0
Helvlaska <ccastan@live.fr>
2025-03-12 16:17:55 +0100

    Create 2-calloc.c file : Program  allocates memory for an array, using malloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - Alloue de la mémoire pour un tableau et initialise à zéro.
+ * @nmemb: Le nombre d'éléments à allouer.
+ * @size: La taille de chaque élément.
+ *
+ * Return: Un pointeur vers le tableau alloué, ou NULL en cas d'échec.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	/*création du tableau*/
+	unsigned int *array;
+	/*varibale pour boucler*/
+	unsigned int i;
+
+	/*vérifie si nmemb ou size est vide*/
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	/*allocation mémoire*/
+	array = malloc(nmemb * size);
+	/*vérifie si l'allocation à réussie*/
+	if (!array)
+		return (NULL);
+	/*boucle pour initialiser à 0 les éléments (nmemb)*/
+	for (i = 0; *array != '\0'; i++)
+		array[i] = 0;
+	return (array);
+}

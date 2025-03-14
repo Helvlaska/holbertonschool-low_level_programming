a0d354d377e6f808fb53f9f321ad7f27af38a9d0
Helvlaska <ccastan@live.fr>
2025-03-14 17:57:49 +0100

    Create dog.h file : Program Define a new type struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure représentant un chien
+ * @name: Nom du chien (chaîne de caractères)
+ * @age: Âge du chien (nombre à virgule)
+ * @owner: Nom du propriétaire (chaîne de caractères)
+ *
+ * Description: Structure contenant des informations sur un chien.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*Fin de la protection*/

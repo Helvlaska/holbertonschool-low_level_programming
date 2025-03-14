bd22cabaa38bd3d1b0df547ccfa8a866bc86ce87
Helvlaska <ccastan@live.fr>
2025-03-14 22:07:16 +0100

    dog.h file : Add a typedef struct name dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -17,6 +17,23 @@ struct dog
 	char *owner;
 };
 
+/**
+ * struct dog - Structure représentant un chien
+ * @name: Nom du chien (chaîne de caractères)
+ * @age: Âge du chien (nombre à virgule)
+ * @owner: Nom du propriétaire (chaîne de caractères)
+ *
+ * Description: Structure contenant des informations sur un chien.
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);

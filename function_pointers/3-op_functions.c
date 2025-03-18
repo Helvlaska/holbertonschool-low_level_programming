d0c931f1ab9a178f46c43444d0ab876a1f258fbf
Helvlaska <ccastan@live.fr>
2025-03-18 14:56:49 +0100

    Create 3-op_functions.c file : Program content 5 function of basic calcul

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,64 @@
+/**
+ * op_add - additionne deux entiers
+ * @a: 1er nombre
+ * @b: 2e nombre
+ *
+ * Return: le resultat
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - soustraie deux entiers
+ * @a: 1er nombre
+ * @b: 2e nombre
+ *
+ * Return: le resultat
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplie deux entiers
+ * @a: 1er nombre
+ * @b: 2e nombre
+ *
+ * Return: le resultat
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divise deux entiers
+ * @a: 1er nombre
+ * @b: 2e nombre
+ *
+ * Return: le resultat
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - division avec reste deux entiers
+ * @a: 1er nombre
+ * @b: 2e nombre
+ *
+ * Return: le resultat
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

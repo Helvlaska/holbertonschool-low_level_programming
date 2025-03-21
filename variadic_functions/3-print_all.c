47ec72a6b70680495e346e80689c38892ef97a43
Helvlaska <ccastan@live.fr>
2025-03-21 22:15:17 +0100

    3-print_all.c file : Betty syntax

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,104 +1,82 @@
 #include "variadic_functions.h"
-#include <stdarg.h> /*bibliothèque pr use variadique fonction*/
-#include <stdio.h> /*bibliothèque pr use printf*/
-#include <stdlib.h> /*bibliothèque pr use NULL*/
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * print_char - Affiche un caractère
  * @arguments: liste d'arguments variadiques
  */
-
 void print_char(va_list arguments)
 {
-	int c = va_arg(arguments, int); /*Les char sont promus en int*/
-
-	printf("%c", c);
+	printf("%c", va_arg(arguments, int));
 }
 
 /**
  * print_int - Affiche un entier
  * @arguments: liste d'arguments variadiques
  */
-
 void print_int(va_list arguments)
 {
-	int n = va_arg(arguments, int);
-
-	printf("%d", n);
+	printf("%d", va_arg(arguments, int));
 }
 
 /**
- * print_float - Affiche un nombre flottant
+ * print_float - Affiche un flottant
  * @arguments: liste d'arguments variadiques
  */
-
 void print_float(va_list arguments)
 {
-	double f = va_arg(arguments, double); /*Les float sont promus en double*/
-
-	printf("%f", f);
+	printf("%f", va_arg(arguments, double));
 }
 
 /**
- * print_string - Affiche une chaîne de caractères, ou (nil) si NULL
+ * print_string - Affiche une chaîne ou (nil)
  * @arguments: liste d'arguments variadiques
  */
-
 void print_string(va_list arguments)
 {
 	char *str = va_arg(arguments, char *);
 
-	if (!str)
+	if (str == NULL)
 		str = "(nil)";
 	printf("%s", str);
 }
 
 /**
- * print_all - Affiche tout type d'arguments selon le format donné
- * @format: chaîne de format contenant les types (c, i, f, s)
- * @...: arguments variadiques à afficher
+ * print_all - Affiche tous les arguments selon le format
+ * @format: chaîne indiquant les types des arguments (c, i, f, s)
  */
-
 void print_all(const char * const format, ...)
 {
-	va_list arguments; /*variable pour lire tout les arguments*/
+	va_list arguments;
+	int i = 0, j;
+	char *sep = "";
 
-	int i = 0, printed; /*variable d'index boucle + booleen*/
+	type_handler handlers[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string}
+	};
 
-	va_start(arguments, format); /*start lecture args*/
-
-	while (format[i]) /*boucle pour parcourir "format"*/
+	va_start(arguments, format);
+	while (format && format[i])
 	{
-		printed = 0; /*réinitialisation à chaque tour*/
-		switch (format[i])
+		j = 0;
+		while (j < 4)
 		{
-			case 'c':
-				print_char(arguments);
-				printed = 1;
+			if (format[i] == handlers[j].symbol)
+			{
+				printf("%s", sep);
+				handlers[j].print_func(arguments);
+				sep = ", ";
 				break;
-			case 'i':
-				print_int(arguments);
-				printed = 1;
-				break;
-			case 'f':
-				print_float(arguments);
-				printed = 1;
-				break;
-			case 's':
-				print_string(arguments);
-				printed = 1;
-				break;
-			default:
-				break;
+			}
+			j++;
 		}
-		/*tant qu'on est pas au dernier argument et bool = true*/
-		if (printed == 1 && format[i + 1] != '\0')
-		{
-			printf(", "); /*on met un separateur*/
-		}
-		i++; /*on avance dans format*/
+		i++;
 	}
-	printf("\n"); /*retour à la ligne*/
-	/*on ferme notre parcours de la liste pour éviter la fuite de mémoire*/
 	va_end(arguments);
+	printf("\n");
 }
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,17 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+/**
+ * struct type_handler - Structure associant un symbole de type
+ * à une fonction d'affichage correspondante
+ * @symbol: caractère représentant le type ('c', 'i', 'f', 's')
+ * @print_func: pointeur vers la fonction de print correspondant au type
+ */
+typedef struct type_handler
+{
+	char symbol;
+	void (*print_func)(va_list);
+} type_handler;
 
 /*****PROTOTYPES DE FONCTION*****/
 int _putchar(char c);

f54217062f47c1f08e75155a7d38e6f2d3c9d54c
Helvlaska <ccastan@live.fr>
2025-04-03 10:59:11 +0200

    :hammer: (4-free_dlistint.c) : Add a description of function

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -4,9 +4,19 @@
 #include <stddef.h>
 #include <string.h>
 
+/**
+ * free_dlistint - Libère la mémoire pour une liste doublement chaînée.
+ * @head: Pointeur vers le début de la liste à libérer.
+ *
+ * Description :
+ * Cette fonction parcourt la liste doublement chaînée en libérant
+ * un à un tous les nœuds, à partir du head jusqu’à la fin.
+ *
+ * Return: Rien.
+ */
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *cursor = head; /*ptr pour parcourir la liste*/
+	dlistint_t *cursor = head; /*ptr pour parcourir la liste*/
 	dlistint_t *noeud_free; /*le noeud à libérer*/
 
 	while (cursor != NULL) /*boucle pour parcourir la liste*/
@@ -15,6 +25,5 @@ void free_dlistint(dlistint_t *head)
 		noeud_free = cursor;
 		cursor = cursor->next; /*on avance avant de libérer*/
 		free(noeud_free); /*on libère le noeud*/
-        head = NULL;
 	}
 }

862eaf013be901da19714ffc346e49c9954be719
Helvlaska <ccastan@live.fr>
2025-04-06 22:52:14 +0200

    :hammer: (3-cp.c) : Add stdlib librarie because use exit() in function

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,7 +1,7 @@
 #include <fcntl.h> /*open() et les flags O_RDONLY, etc*/
 #include <unistd.h> /*pour read(), write(), close(), STDOUT_FILENO*/
 #include <stdio.h> /*pour dprintf()*/
-
+#include <stdlib.h> /*pour malloc(), free()*/
 /**
  * main - copie le contenu d’un fichier dans un autre
  * @argc: nombre d’arguments
